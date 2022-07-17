8ce0564fc5d941be6ec44683f187db068e2c8e5e
Jaroslaw Charchula <charchula@gmail.com>
2022-07-18 01:13:36 +0200

    add SharedLayout page code refactor

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
 import Error from './pages/Error';
+import ShardeLayout from './pages/SharedLayout'
 
 
 function App() {
   return (
   <BrowserRouter>  
     <Routes>
-      <Route path='/' element={<Home />} >
+      <Route path='/' element={<ShardeLayout />} >
+        <Route index element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/products' element={<Products />} />
         <Route path='*' element={<Error />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Link; } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/products'>Products</Link>
+    </nav>
   )
 }
 
