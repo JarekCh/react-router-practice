90d63f2958d11df7e365c1b3e5fd130153afedbc
Jaroslaw Charchula <charchula@gmail.com>
2022-07-18 01:56:39 +0200

    add StyledNavba as example. add navigate to single product

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,21 @@
+import products from '../data'
+import { Link } from 'react-router-dom';
+
 const Products = () => {
-  return (
-    <>
+  return (    
       <section className='section'>
         <h2>products</h2>
-      </section>
-    </>
+        <div className='products'>
+          {products.map((product) => {
+            return (
+              <article key={product.id}>
+                <h5>{product.name}</h5>
+                <Link to={`/products/${product.id}`}>more info</Link>
+              </article>
+            );
+          })}
+        </div>
+      </section>    
   );
 };
 
